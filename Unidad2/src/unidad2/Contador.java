936488b0d4d5a080510996cf9a1855b053199497
enelsotillo <enelsotillo@gmail.com>
2022-01-22 13:47:36 -0300

    Delete Unidad2 directory

--- Unidad2/src/unidad2/Contador.java
+++
@@ -1,9 +0,0 @@
-//Unidad 2 ejercicio 3
-package unidad2;
-public class Contador {
-    int Cont;
-    //metodo construtor
-public Contador(){
-    Cont++;
-}
-}
--- Unidad2/src/unidad2/Unidad2.java
+++
@@ -1,9 +0,0 @@
-
-package unidad2;
-public class Unidad2 {
-
-    public static void main(String[] args) {
-
-    }
-    
-}
